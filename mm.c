7f54cb6757046e39efa8bac79ebcd53b85db0ad5
longg2610 <phamduylong26102004@gmail.com>
2025-04-29 22:05:15 -0400

    segregated free list implementation, still lots of fragmentation but this will do for educational purpose

--- mm.c
+++ mm.c
@@ -45,13 +45,12 @@ team_t team = {
 #define ALLOCATED 1
 #define FREE 0
 #define MINBLOCKSIZE (SIZE_T_SIZE + SIZE_T_SIZE + SIZE_T_SIZE + SIZE_T_SIZE)
-#define BINCOUNT 10
+#define BINCOUNT 20
 
 size_t* bins [BINCOUNT];    /*segregated free list bins*/
-size_t* free_list_h;  /*point to the HEADER of the first free block*/
 
 /*helper functions*/
-void print_free_list();
+void print_free_list(size_t* free_list_h);
 size_t blockSize(size_t* head){return (*head) & (~0x7);}
 size_t allocStatus(size_t* head){return (*head) & (0x1);}
 void setBlockSize(size_t* head, size_t size)
@@ -84,6 +83,7 @@ void setSucc(size_t* head, size_t* ptr){*(head + 2) = ptr;}
 size_t* nextBlock(size_t *head){return head + blockSize(head) / SIZE_T_SIZE;}
 size_t* prevBlock(size_t* head){return head - blockSize(head - 1) / SIZE_T_SIZE;}
 int atEpilogue(size_t* ptr){return (allocStatus(ptr) == ALLOCATED) && (blockSize(ptr) == 0);}
+int endOfList(size_t* block){return (getSucc(block) == NULL);}
 void changeHead(size_t* head, size_t size, int alloc_op)
 {
     *head = size;
@@ -94,25 +94,12 @@ void changeHead(size_t* head, size_t size, int alloc_op)
 }
 void copyHeadToFoot(size_t* header){*(header + blockSize(header) / SIZE_T_SIZE - 1) = *header;}
 
-
 /*key functions' signatures*/
 void* coalesce(size_t* to_free);
 void split(size_t total, size_t taken, size_t* taken_blk);
 void splice(size_t* block);
 void insertFreeBlock(size_t* free_list, size_t* block);
-int findBin(size_t size)
-{
-    if (size <= 16) return 0;
-    if (size <= 32) return 1;
-    if (size <= 64) return 2;
-    if (size <= 128) return 3;
-    if (size <= 256) return 4;
-    if (size <= 512) return 5;
-    if (size <= 1024) return 6;
-    if (size <= 2048) return 7;
-    if (size <= 4096) return 8;
-    return 9;
-}
+int findBin(size_t size);
 
 /*
  * mm_init - initialize the malloc package.
@@ -120,7 +107,8 @@ int findBin(size_t size)
 int mm_init(void)
 {
     mem_init();    
-    mem_sbrk(MINBLOCKSIZE * BINCOUNT + SIZE_T_SIZE);  /*4 for head, foot, pred, succ of sentinel, BINCOUNT of those for BINCOUNT free lists. plus one for shared epilogue. */
+    /*4 for head, foot, pred, succ of sentinel, 2 sentinels head and tail, BINCOUNT of those for BINCOUNT free lists. plus one for epilogue*/
+    mem_sbrk(MINBLOCKSIZE * 2 * BINCOUNT + SIZE_T_SIZE); 
     
     /*initialize the epilogue block*/
     size_t* epilogue = (size_t *)mem_heap_hi();
@@ -130,14 +118,18 @@ int mm_init(void)
 
     for(int i = 0; i < BINCOUNT; i++)
     {
-        bins[i] = ((size_t*) mem_heap_lo()) + MINBLOCKSIZE * i;   
-        setBlockSize(bins[i], MINBLOCKSIZE);  /*sentinel block*/
+        bins[i] = ((size_t*) mem_heap_lo()) + (MINBLOCKSIZE * 2 * i) / SIZE_T_SIZE;   
+        setBlockSize(bins[i], MINBLOCKSIZE);  /*sentinel head*/
         setAllocStatus(bins[i], ALLOCATED);
         setPred(bins[i], NULL);
-        setSucc(bins[i], epilogue);
-    }
-    
+        setSucc(bins[i], nextBlock(bins[i]));
 
+        size_t* free_list_t = nextBlock(bins[i]);
+        setBlockSize(free_list_t, MINBLOCKSIZE);  /*sentinel tail*/
+        setAllocStatus(free_list_t, ALLOCATED);
+        setPred(free_list_t, bins[i]);
+        setSucc(free_list_t, NULL);
+    }
     return 0;
 }
 
@@ -150,15 +142,12 @@ void *mm_malloc(size_t size)
     if(size == 0)        /*spurious requests*/
         return NULL; 
     int newsize = ALIGN(size + SIZE_T_SIZE + SIZE_T_SIZE + SIZE_T_SIZE + SIZE_T_SIZE); /*header, footer, pred, succ*/
+    int starting_bin = findBin(newsize);
 
-    int starting_bin = findBin(size);
-
-    size_t* ptr;
     for (int b = starting_bin; b < BINCOUNT; b++)
     {
-        /*size_t* ptr = free_list_h;*/
-        ptr = bins[b];      /*init will have to initialize all free lists with head sentinel and epilogue*/    
-        while (!(atEpilogue(ptr)) &&                                   /*boundary check*/
+        size_t* ptr = bins[b]; 
+        while (!(endOfList(ptr)) &&                                   /*boundary check*/
             (allocStatus(ptr) == ALLOCATED ||                           /*while not yet found a free block*/
                 blockSize(ptr) < newsize))                                 /*or block does not fit*/
         {
@@ -171,31 +160,25 @@ void *mm_malloc(size_t size)
             split(blockSize(ptr), newsize, ptr);
             return (void *)((size_t*)ptr + 1); /*return start of payload*/
         }
-        else if (atEpilogue(ptr)) 
-            continue; /*for clarity*/
-    }
-    
-    /*(if check wont be needed here cuz ptr will be at epilogue when for loop ends)*/
-    if (atEpilogue(ptr))           /*no free space found, request more VM*/
-    {
-        void* new_mem = mem_sbrk(newsize); 
-        if (new_mem == (void *)-1)
-            return NULL;
-        else
-        {
-            setBlockSize(new_mem, newsize);
-            setAllocStatus(new_mem, FREE);      /*new mem free, coalesce*/
-            
-            /*new epilogue*/
-            *(nextBlock(new_mem)) = 0x1;
-            
-            new_mem = (size_t*) (coalesce(new_mem));
-            split(blockSize(new_mem), newsize, new_mem);
-            
-            return (void *)((size_t*)(new_mem) + 1); /*return start of payload*/
-        }
+        
     }
 
+
+    void* new_mem = mem_sbrk(newsize); 
+    if (new_mem == (void *)-1)
+        return NULL;
+    else
+    {
+        setBlockSize(new_mem, newsize);
+        setAllocStatus(new_mem, FREE);      /*new mem free, coalesce*/
+        
+        /*new epilogue*/
+        *(nextBlock(new_mem)) = 0x1;
+        
+        new_mem = (size_t*) (coalesce(new_mem));
+        split(blockSize(new_mem), newsize, new_mem);
+        return (void *)((size_t*)(new_mem) + 1); /*return start of payload*/
+    }
 }
 
 
@@ -369,8 +352,7 @@ void insertFreeBlock(size_t* free_list, size_t* block)
 {
     size_t* cur_first_block = getSucc(free_list);
     setSucc(block, cur_first_block);
-    if(!atEpilogue(cur_first_block))
-        setPred(cur_first_block, block);
+    setPred(cur_first_block, block);
     setSucc(free_list, block);
     setPred(block, free_list);
 }
@@ -380,28 +362,43 @@ void splice(size_t* block)
     size_t* pred = getPred(block);
     size_t* succ = getSucc(block);
     setSucc(pred, succ);
-    if(!atEpilogue(succ))
-        setPred(succ, pred);
+    setPred(succ, pred);
 }
 
-void print_free_list()
+int findBin(size_t size)
+{
+    if (size <= 16) return 0;
+    if (size <= 32) return 1;
+    if (size <= 64) return 2;
+    if (size <= 128) return 3;
+    if (size <= 256) return 4;
+    if (size <= 512) return 5;
+    if (size <= 1024) return 6;
+    if (size <= 2048) return 7;
+    if (size <= 4096) return 8;
+    if (size <= 8192) return 9;
+    if (size <= 16384) return 10;
+    if (size <= 32768) return 11;
+    if (size <= 65536) return 12;
+    if (size <= 131072) return 13;
+    if (size <= 262144) return 14;
+    if (size <= 524288) return 15;
+    if (size <= 1048576) return 16;
+    if (size <= 2097152) return 17;
+    if (size <= 4194304) return 18;
+    return 19;  // fallback bin for very large blocks
+}
+
+void print_free_list(size_t* free_list_h)
 {
     size_t* ptr = free_list_h;
-    while (!atEpilogue(ptr))
+    while (!endOfList(ptr))
     {
         printf("%d -> ",ptr);
         ptr = getSucc(ptr);
     }
-    printf("EPL\n");
+    printf("EOL\n");
 }
-
-int mm_check(void)
-{
-
-    
-}
-
-
 
 /*MALLOC-SPLIT FOR ADDRESS-ORDERED FREE LIST*/
 /*size_t* prev_free = getPred(taken_blk);
